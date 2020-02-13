128f38654895cb3608c22169f5938304da616639
grastello <gabriele.rastello@edu.unito.it>
2020-02-13 18:30:12 +0100

    Use Leuven

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -29,7 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes nil)
+ '(custom-enabled-themes (quote (leuven)))
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
